e9fea82d0f4031e33037d8d56f397177a2ee1cd0
Sean Stevens <sstevens88@gmail.com>
2017-06-09 21:56:37 -0700

    Corrected excess whitespace and formatting

--- static/scripts.js
+++ static/scripts.js
@@ -2,29 +2,30 @@
 
 var observe;
 if (window.attachEvent) {
-    observe = function (element, event, handler) {
-        element.attachEvent('on'+event, handler);
+    observe = function(element, event, handler) {
+        element.attachEvent('on' + event, handler);
     };
-}
-else {
-    observe = function (element, event, handler) {
+} else {
+    observe = function(element, event, handler) {
         element.addEventListener(event, handler, false);
     };
 }
-function init () {
+
+function init() {
     var text = document.getElementById('text');
-    function resize () {
+
+    function resize() {
         text.style.height = 'auto';
-        text.style.height = text.scrollHeight+'px';
+        text.style.height = text.scrollHeight + 'px';
     }
-/* 0-timeout to get the already changed text */
-    function delayedResize () {
+    /* 0-timeout to get the already changed text */
+    function delayedResize() {
         window.setTimeout(resize, 0);
     }
-    observe(text, 'change',  resize);
-    observe(text, 'cut',     delayedResize);
-    observe(text, 'paste',   delayedResize);
-    observe(text, 'drop',    delayedResize);
+    observe(text, 'change', resize);
+    observe(text, 'cut', delayedResize);
+    observe(text, 'paste', delayedResize);
+    observe(text, 'drop', delayedResize);
     observe(text, 'keydown', delayedResize);
 
     resize();
